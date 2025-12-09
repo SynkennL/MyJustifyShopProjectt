4fcdb2c6da8cc09772c99d70bb36a6779ce7f537
SynkennL <Asyncleynn@hotmail.com>
2025-12-10 01:30:41 +0300

    Update

--- frontend/src/views/CompareView.vue
+++ frontend/src/views/CompareView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { useRouter } from "vue-router";
 import { compareList, removeFromCompare, clearCompare } from "../services/compare";
 import { addToCart } from "../services/cart";
